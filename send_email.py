eabff128d73107b90e2a706dabec98b1b8035d15
chetan2305 <gurramchetan2305@gmail.com>
2025-08-22 22:09:19 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,1 +1,16 @@
+# Module to send email using Gmail API
+from googleapiclient.discovery import build
+import base64
+from email.mime.text import MIMEText
+
+def create_message(to, subject, message_text):
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['subject'] = subject
+    raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    return {'raw': raw}
+
+def send_message(service, message):
+    sent_message = service.users().messages().send(userId='me', body=message).execute()
+    return sent_message
 
